e3c4bc2032b0d0205bf4353e911a705e8ce4547a
Lorenzo Signoretti <lory4846@hotmail.it>
2022-06-28 13:54:52 +0200

    Create GameMap

---
+++ LorenzoSignoretti/Tasks/GameMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class GameMap : BaseEntity
+    {
+        public List<Block> Blocks { get; set; }
+        public GameMap() : base(new Point2D(), true)
+        {
+            Blocks = new List<Block>();
+        }
+        public void AddBlock(Block block)
+        {
+            Blocks.Add(block);
+        }
+        public void AddBlocks(List<Block> blocks)
+        {
+            Blocks.AddRange(blocks);
+        }
+
+    }
+}
